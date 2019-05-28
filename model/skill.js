a77032f394fa09c4ebb60458243bce745701dec8
HardPlant <abc7988se@naver.com>
2019-05-28 17:48:37 +0900

    added type

--- model/skill.js
+++ model/skill.js
@@ -10,6 +10,26 @@ const DamageDealer = {
         } else if (this.type === Type.Special) {
             destDoll.HP -= this.amount;
         }
+    },
+    physical : function(srcDoll, destDoll) {
+        if (this.type === Type.Physical) {
+            destDoll.HP -= this.amount;
+        } else if (this.type === Type.Special) {
+            destDoll.HP -= this.amount;
+        }
+    },
+    special : function(srcDoll, destDoll) {
+        if (this.type === Type.Physical) {
+            destDoll.HP -= this.amount;
+        } else if (this.type === Type.Special) {
+            destDoll.HP -= this.amount;
+        }
+    },
+    selectAuto: function(type) {
+        if (type === Type.Physical) return DamageDealer.physical;
+        if (type === Type.Special) return DamageDealer.special;
+        return DamageDealer.pure
+
     }
 }
 const Skill = {
@@ -20,11 +40,11 @@ const Skill = {
         this.PP = 35;
     },
     effect: {
-        Damage: function(type, amount) {
+        Damage: function(type, amount, dealer) {
             this.type = type || Type.Physical; // Physical, Special, Alter
             this.amount = amount || 0;
 
-            this.apply = DamageDealer.pure;
+            this.apply = dealer || DamageDealer.selectAuto(this.type);
         },
         Status: function() {
             
