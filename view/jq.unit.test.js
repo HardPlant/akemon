b5c23ee7f578a1cb22a9cec411ac16c3c300c48c
HardPlant <abc7988se@naver.com>
2019-06-04 19:34:42 +0900

    update

---
+++ view/jq.unit.test.js
@@ -0,0 +1,16 @@
+var $ = require("jquery");
+var html = require("fs").readFileSync("./index.html").toString();
+
+document.documentElement.innerHTML = html;
+
+// and you can test like..
+// $("#").hasClass();
+// $("#").val();
+// expect($("#").hasClass()).toBe(true);
+
+describe("validateSubmits", function() {
+    it("shows/hides error banner", function() {
+
+    });
+
+});
