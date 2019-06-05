8f848d7b05a5686130a8a022c73389634f37b3f5
HardPlant <abc7988se@naver.com>
2019-06-05 19:58:04 +0900

    addPercent

--- view/doll_div.js
+++ view/doll_div.js
@@ -16,10 +16,11 @@ Doll_Div.Enemy = function(idol) {
 
 Doll_Div.faint = function(doll_div) {
     this.whiteArea = document.createElement("div");
-    this.whiteArea.css("z-index", "10");
-    $(this.whiteArea).animate(
-        {""}
-    )
+    this.whiteArea.style.zIndex = "10";
+    this.whiteArea.style.backgroundColor = "white";
+    this.whiteArea.style.top = addPercent(doll_div.style.top, "10%");
+    doll_div.css("top");
+    $(this.whiteArea).animate({top: "10%", left: "10%"});
 };
 
 Doll_Div.withdraw = function(doll_div) {
@@ -39,3 +40,11 @@ Doll_Div.effect.lvlUp = function(doll_div) {
 Doll_Div.effect.abilityChange = function(doll_div) {
 
 };
+
+function addPercent(from, to) {
+    var a = Number.parseInt(from);
+    var b = Number.parseInt(to);
+    var result = "" + (a + b) + "%";
+
+    return result;
+}
