ac022d7ecb44a0c94f84bbf8be47e22e41031c7c
HardPlant <abc7988se@naver.com>
2019-06-07 20:21:31 +0900

    red, priority is broken

--- model/idolbattle.unit.test.js
+++ model/idolbattle.unit.test.js
@@ -62,3 +62,56 @@ describe("init flow", ()=> {
 
     });
 });
+
+describe("tab battle", ()=> {
+    var playerIdol1 = new Idol.Idol();
+    playerIdol1.HP = 30;
+    playerIdol1.SPD = 1;
+    playerIdol1.SkillList = [skill_idx.getBaseByIdx(0)];
+
+    var playerIdol2 = new Idol.Idol();
+    playerIdol1.HP = 30;
+    playerIdol1.SPD = 3;
+    playerIdol1.SkillList = [skill_idx.getBaseByIdx(0)];
+
+    var enemyIdol1 = new Idol.Idol();
+    enemyIdol1.HP = 30;
+    enemyIdol1.SPD = 2;
+    enemyIdol1.SkillList = [skill_idx.getBaseByIdx(1)];
+
+    var enemyIdol2 = new Idol.Idol();
+    enemyIdol1.HP = 30;
+    enemyIdol1.SPD = 4;
+    enemyIdol1.SkillList = [skill_idx.getBaseByIdx(1)];
+
+    const battle = new IdolBattle.IdolBattle();
+    var priority = [];
+    var currentDoll = {}; // Idol.Idol;
+    var plan = {
+        srcDoll: {},
+        destDoll: {},
+        skill: {}
+    };
+
+    test("Init", () => {
+        var playerSet = [playerIdol1, playerIdol2];
+        var enemySet = [enemyIdol1, enemyIdol2];
+
+        battle.setPlayer(playerSet);
+        battle.setEnemy(enemySet);
+
+        expect(battle.dolls).toContain(playerIdol1);
+        expect(battle.dolls).toContain(enemyIdol1);
+    });
+
+    test("get Doll priority", ()=> {
+        priority = IdolBattle.getDollPriorityBySpeed(battle);
+        battle.priority = priority;
+
+        expect(priority[0]).toBe(enemyIdol2);
+        expect(priority[1]).toBe(playerIdol2);
+        expect(priority[2]).toBe(enemyIdol1);
+        expect(priority[3]).toBe(playerIdol1);
+    });
+
+});
