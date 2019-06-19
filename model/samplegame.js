6b11c5d60d09166b096af0ee24185abc7c81ebe2
HardPlant <abc7988se@naver.com>
2019-06-19 20:40:18 +0900

    test cases

---
+++ model/samplegame.js
@@ -0,0 +1,4 @@
+const Idol = require("./idol");
+const IdolBattle = require("./idolbattle");
+const Skill = require("./skill");
+const Skill_idx = require("./skill_idx");
---
+++ model/samplegame.unit.test.js
@@ -0,0 +1,25 @@
+
+
+describe("pre-game phase", ()=> {
+    it("show units", ()=>{
+        
+    });
+    it("choose unit for me", ()=> {
+
+    });
+    it("choose unit for enemy", ()=>{
+
+    });
+    it("choose difficulty", ()=>{
+
+    });
+    it("start game", ()=> {
+
+    });
+});
+
+describe("in-game phase", ()=> {
+
+});
+
+describe()
