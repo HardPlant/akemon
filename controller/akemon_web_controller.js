c26ede8e8504b01589b1c1d38a709108d4372f0f
HardPlant <abc7988se@naver.com>
2019-06-20 18:05:11 +0900

    clean.

--- controller/akemon_web_controller.js
+++ controller/akemon_web_controller.js
@@ -1,13 +1,42 @@
-function akemonBattleController() {
-    init: function() {
+const express = require("express");
+function akemonBattleController(app) {
+    
+    /*
+    * GET /start
+    * ========
+    *
+    */
 
-    },
-    reset: function() {
+    app.post("/", start);
+    function start(req, res) {
+        return res.json({
+            result: "hello world"
+        });
+    };
 
-    },
-    progress: function() {
+    /*
+    *
+    *
+    *
+    */
+    app.post("/progress", progress);
+    function progress(req, res) {
+        var result = {};
 
-    }
+        res.json(result);
+    };
 }
 
-module.exports.akemonController = akemonController;
+function standalone() {
+    var app = express();
+    
+    akemonBattleController(app);
+
+    var server = app.listen(3000, function(){
+        console.log("Express server has started on port 3000")
+    });
+}
+
+module.exports.akemonBattleController = akemonBattleController;
+
+standalone();
--- controller/akemon_web_controller.unit.test.js
+++ controller/akemon_web_controller.unit.test.js
@@ -1,3 +1,24 @@
-describe("", ()=> {
+const request = require("request");
 
+describe("start", ()=> {
+    var options;
+
+    beforeAll(()=>{
+        options = {
+            uri: 'http://localhost:3000/',
+            method: 'POST',
+            json: true,
+            body: {
+
+            }
+        };
+    });
+
+    it("starts", (done)=> {
+        request.post(options, (err, response, body)=>{
+
+            expect(response.body.result).toBe("hello world");
+            done();
+        });
+    });
 });
