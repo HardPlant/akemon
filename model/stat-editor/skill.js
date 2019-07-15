ee95786e66c1c6671a8c6a75e3f5dae7dc847725
HardPlant <abc7988se@naver.com>
2019-07-15 11:34:32 +0900

    Feature multitype (#23)
    * Update skill.js
    
    * Update and rename skill.unit.test to skill.unit.test.js

--- model/stat-editor/skill.js
+++ model/stat-editor/skill.js
@@ -18,16 +18,55 @@ function calculateModifier(skill, battle, srcDoll, destDoll) {
     var totalModifier = [1];
     var criticalness = battle.getDefaultCritical() + skill.bonusCritical;
     totalModifier.push(getCriticalModifier(criticalness));
-    totalModifier.push(getTypeModifier(battle, srcDoll, destDoll));
+    totalModifier.push(getSelfTypeModifier(skill, srcDoll));
+    totalModifier.push(getTypeModifier(skill, battle, srcDoll, destDoll));
 
     return totalModifier;
 }
 
 function getCriticalModifier(criticalness) {
     if (Math.random() < criticalness) return 1.5;
-    else return 1;
-}
-
-function getTypeModifier() {
     return 1;
+}
+function getSelfTypeModifier(skill, srcDoll) {
+    if (!srcDoll.type) return 1;
+    if (srcDoll.type.indexOf(skill.type) > -1) 
+        return 1.5;
+    return 1;
+}
+function getTypeModifier(skill, battle, srcDoll, destDoll) {
+    if (!(destDoll.type && skill.type )) return 1;
+    // Attack => Defense
+    var testTable = {
+        "Water": {
+            "Water" : 0.5,
+            "Ice" : 1,
+            "Dragon" : 0.5,
+            "Flying" : 1,
+        },
+        "Ice": {
+            "Water" : 0.5,
+            "Ice" : 0.5,
+            "Dragon" : 2,
+            "Flying" : 2,
+        },
+        "Dragon": {
+            "Water" : 1,
+            "Ice" : 1,
+            "Dragon" : 2,
+            "Flying" : 1,
+        },
+        "Flying": {
+            "Water" : 1,
+            "Ice" : 1,
+            "Dragon" : 1,
+            "Flying" : 1,
+        },
+    }
+    var modifiers = [1];
+    destDoll.type.forEach(function(defenseType) {
+        modifiers.push(testTable[skill.type][defenseType])
+    });
+
+    return modifiers.reduce(multipleReduce);
 }
