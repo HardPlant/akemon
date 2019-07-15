5da0e38e019a506dc58209a19c09636fbcfd1f28
HardPlant <abc7988se@naver.com>
2019-07-15 14:26:05 +0900

    Revert "Feature fulltype (#25)" (#26)
    This reverts commit 638bb47f373d0211b079c072aa84d36bb69285e1.

--- model/skill.unit.test.js
+++ model/skill.unit.test.js
@@ -1,160 +1,303 @@
-eval(require("fs").readFileSync("model/stat-editor/stat-editor.js")+"");
-eval(require("fs").readFileSync("model/stat-editor/skill.js")+"");
+const Idol = require('./idol');
+const Skill = require("./skill");
+const skill_idx = require("./skill_idx");
+const IdolBattle = require("./idolbattle");
 
-var battle;
+jest.mock("skill_idx");
 
-beforeAll(function(){
-    battle = {};
-    battle.getSkillRandom = function() {
-        return 1;
-    };
-    battle.getDefaultCritical = function() {
-        return 0;
-    };
-    battle.weather = {};
-});
-
-it("deals damage", function() {
-    var srcDoll = getBalancedDoll();
-    var destDoll = getBalancedDoll();
-    var skill = getSkillBase();
-
-    var resultDamage = skillDamage(skill, battle, srcDoll, destDoll);
-    expect(typeof(resultDamage)).toBe("number");
-    console.log(resultDamage);
-});
-
-it("deals damage", function() {
-    var srcDoll = getBalancedDoll();
-    var destDoll = getDefenseDoll();
-    var skill = getSkillBase();
-
-    var resultDamage = skillDamage(skill, battle, srcDoll, destDoll);
-    expect(typeof(resultDamage)).toBe("number");
-    console.log(resultDamage);
-});
-it("deals damage", function() {
-    var srcDoll = getAttackDoll();
-    var destDoll = getBalancedDoll();
-    var skill = getSkillBase();
-
-    var resultDamage = skillDamage(skill, battle, srcDoll, destDoll);
-    expect(typeof(resultDamage)).toBe("number");
-    console.log(resultDamage);
-});
-it("criticals", function() { 
-    var critBattle = Object.create(battle);
-    critBattle.getDefaultCritical = function() {
-        return 1;
-    };
-    srcDoll = getBalancedDoll();
-    destDoll = getBalancedDoll();
-    skill = getSkillBase();
-
-    var modifier = calculateModifier(skill, critBattle, srcDoll, destDoll);
-    expect(modifier[1]).toBe(1.5);
-});
-it("bonuses self-type", function() { 
-    var critBattle = Object.create(battle);
-    critBattle.getDefaultCritical = function() {
-        return 1;
-    };
-    srcDoll = getBalancedDoll();
-    destDoll = getBalancedDoll();
-    skill = getSkillBase();
-
-    skill.type = "Ice";
-    srcDoll.type = ["Ice"];
-
-    var modifier = calculateModifier(skill, critBattle, srcDoll, destDoll);
-    expect(modifier[2]).toBe(1.5);
-});
-it("halves again weak type", function() {
-    srcDoll = getBalancedDoll();
-    destDoll = getBalancedDoll();
-    skill = getSkillBase();
-
-    skill.type = "Ice";
-    destDoll.type = ["Water"];
-
-    var modifier = calculateModifier(skill, battle, srcDoll, destDoll);
-    console.log(modifier);
-    expect(modifier[3]).toBe(0.5);
-});
-it("doubles again strong type", function() {
-    srcDoll = getBalancedDoll();
-    destDoll = getBalancedDoll();
-    skill = getSkillBase();
-
-    skill.type = "Ice";
-    destDoll.type = ["Dragon"];
-
-    var modifier = calculateModifier(skill, battle, srcDoll, destDoll);
-    console.log(modifier);
-    expect(modifier[3]).toBe(2);
-});
-function getBalancedDoll() {
-    var stat = new BaseStat({
-        LV: 50,
-        HP: 100,
-        ATK: 100,
-        DEF: 100,
-        SAT: 100,
-        SDF: 100,
-        SPD: 100        
+describe("creation test", ()=> {
+    test("init", ()=> {
+        var skill = new Skill.Skill();
+        skill.effects.push(new Skill.effect.Damage(Skill.DamageType.Pure, 120));
     });
-    var doll = {};
-    doll.baseStat = stat;
-    doll.ability = {};
-    doll.statModifiers = {};
-    doll.stat = realStat(doll, battle);
+    test("init from idx", ()=> {
+        var skill = skill_idx.getBaseByIdx(0);
 
-    return doll;
-}
-function getAttackDoll() {
-    var stat = new BaseStat({
-        LV: 50,
-        HP: 100,
-        ATK: 150,
-        DEF: 50,
-        SAT: 150,
-        SDF: 50,
-        SPD: 100        
+        expect(skill).not.toBeUndefined();
+        expect(skill.name).not.toBeUndefined();
+        expect(skill.name).toBe("0번 스킬");
     });
-    var doll = {};
-    doll.baseStat = stat;
-    doll.ability = {};
-    doll.statModifiers = {};
-    doll.stat = realStat(doll, battle);
+})
 
-    return doll;
-}
-function getDefenseDoll() {
-    var stat = new BaseStat({
-        LV: 50,
-        HP: 100,
-        ATK: 50,
-        DEF: 150,
-        SAT: 50,
-        SDF: 150,
-        SPD: 100        
+describe("interact with self", ()=> {
+    var destIdol = new Idol.Idol();
+    destIdol.HP = 10;
+    destIdol.LV = 100;
+
+    test("deal damage to self", ()=> {
+        var skill = new Skill.Skill();
+
+        skill.effects.push(new Skill.effect.Damage(Skill.DamageType.Pure,120));
+
+        Skill.apply(skill, destIdol, destIdol);
+        expect(destIdol.HP).not.toBe(10);
     });
-    var doll = {};
-    doll.baseStat = stat;
-    doll.ability = {};
-    doll.statModifiers = {};
-    doll.stat = realStat(doll, battle);
-    return doll;
-}
-function getSkillBase() {
-    var skillBase = {
-        power: 90,
-        type: "Normal",
-        srcStat: "ATK",
-        destStat: "DEF",
-        bonusCritical: 0,
-        beforeHit: function(){},
-        afterHit: function(){}
-    };
-    return skillBase;
-}
+
+    test("deal status", ()=> {
+        var skill = new Skill.Skill();
+        skill.effects.push(new Skill.effect.Status(Skill.StatusType.Poison));
+        
+        Skill.apply(skill, destIdol, destIdol);
+        expect(destIdol.statusList.length).not.toBe(0);
+    });
+});
+
+describe("interact with other idol", ()=> {
+    var srcIdol = new Idol.Idol();
+    srcIdol.HP = 10;
+
+    var destIdol = new Idol.Idol();
+    destIdol.HP = 10;
+
+    srcIdol.LV = 100;
+    destIdol.LV = 100;
+
+    test("dealing damage test", ()=> {
+        var skill = new Skill.Skill();
+
+        skill.effects.push(
+            new Skill.effect.Damage(Skill.DamageType.Pure,120));
+        Skill.apply(skill, srcIdol, destIdol);
+        expect(destIdol.HP).not.toBe(10);
+        expect(destIdol.HP).toBe(-110);
+    });
+    
+    test("dealing status test", ()=> {
+        //plan = Idol.selectSkillAndTarget(battle, currentDoll);
+    });
+});
+
+describe("type test", ()=> {
+    var srcIdol;
+    var destIdol;
+    var skill;
+    var damage;
+    
+    beforeEach(()=> {
+        srcIdol = new Idol.Idol();
+        srcIdol.HP = 10;
+    
+        destIdol = new Idol.Idol();
+        destIdol.HP = 10;
+
+        srcIdol.LV = 100;
+        destIdol.LV = 100;
+    });
+
+    test("normal", ()=> {
+        skill = new Skill.Skill();
+        skill.attrType = "Normal"
+        skill.effects.push(new Skill.effect.Damage(Skill.DamageType.Pure,5, skill));
+        destIdol.attrType = ["Normal"];
+
+        Skill.apply(skill, srcIdol, destIdol);
+
+        expect(destIdol.HP).toBe(5);
+    });
+    
+    test("effective", ()=> {
+        skill = new Skill.Skill();
+        skill.attrType = "Effective";
+        skill.effects.push(new Skill.effect.Damage(Skill.DamageType.Pure,5, skill));
+        destIdol.attrType = ["Effective"];
+        
+        expect(destIdol.attrType).not.toBe(undefined);
+        Skill.apply(skill, srcIdol, destIdol);
+        
+        expect(destIdol.HP).toBe(10 - (5 * 2));
+    });
+
+    test("double effective", ()=> {
+        skill = new Skill.Skill();
+        skill.attrType = "Effective";
+        skill.effects.push(new Skill.effect.Damage(Skill.DamageType.Pure,5, skill));
+        destIdol.attrType = ["Effective", "Effective"];
+        
+        expect(destIdol.attrType).not.toBe(undefined);
+        Skill.apply(skill, srcIdol, destIdol);
+        
+        expect(destIdol.HP).toBe(10 - (5 * 2 * 2));
+    });
+
+    test("selftyped", ()=> {
+        skill = new Skill.Skill();
+        skill.attrType = "Effective";
+        skill.effects.push(new Skill.effect.Damage(Skill.DamageType.Pure,5, skill));
+        srcIdol.attrType = "Effective"
+        destIdol.attrType = ["Normal"];
+
+        Skill.apply(skill, srcIdol, destIdol);
+
+        expect(destIdol.HP).toBe(10 - (5 * 1.5));
+    });
+
+    test("selftyped effective", ()=> {
+        skill = new Skill.Skill();
+        skill.attrType = "Effective";
+        skill.effects.push(new Skill.effect.Damage(Skill.DamageType.Pure,5, skill));
+        srcIdol.attrType = "Effective";
+        destIdol.attrType = ["Effective"];
+
+        Skill.apply(skill, srcIdol, destIdol);
+
+        expect(destIdol.HP).toBe(10 - (5 * 2 * 1.5));
+    });
+
+    test("selftyped noneffective", ()=> {
+        skill = new Skill.Skill();
+        skill.attrType = "Effective";
+        skill.effects.push(new Skill.effect.Damage(Skill.DamageType.Pure,5, skill));
+        srcIdol.attrType = "Effective";
+        destIdol.attrType = ["NonEffective"];
+
+        Skill.apply(skill, srcIdol, destIdol);
+
+        expect(destIdol.HP).toBe(10 - (5 * 1.5 * 0.5));
+    });
+
+    test("not effective", ()=> {
+        skill = new Skill.Skill();
+        skill.effects.push(new Skill.effect.Damage(Skill.DamageType.Pure,5, skill));
+        skill.attrType = "NonEffective";
+        destIdol.attrType = ["NonEffective"];
+        
+        expect(destIdol.attrType).not.toBe(undefined);
+        Skill.apply(skill, srcIdol, destIdol);
+        
+        expect(destIdol.HP).toBe(7.5);
+    });
+
+    test("lv10", ()=> {
+        skill = new Skill.Skill();
+        skill.effects.push(new Skill.effect.Damage(Skill.DamageType.Physical,5, skill));
+        skill.attrType = "Normal";
+
+        srcIdol.LV = 10;
+        srcIdol.ATK = 0;
+        destIdol.DEF = 0;
+
+        destIdol.attrType = ["Normal"];
+        expect(destIdol.attrType).not.toBe(undefined);
+        
+        Skill.apply(skill, srcIdol, destIdol);
+        expect(destIdol.HP).toBe(9);
+    });
+    test("lv50", ()=> {
+        skill = new Skill.Skill();
+        skill.effects.push(new Skill.effect.Damage(Skill.DamageType.Physical,5, skill));
+        skill.attrType = "Normal";
+
+        srcIdol.LV = 50;
+        srcIdol.ATK = 0;
+        expect(srcIdol.stats).not.toBe(undefined);
+        destIdol.DEF = 0;
+
+        destIdol.attrType = ["Normal"];
+        expect(destIdol.attrType).not.toBe(undefined);
+        
+        Skill.apply(skill, srcIdol, destIdol);
+
+        expect(skill.applyResult["amount"]).toBe(2.5);
+        expect(destIdol.HP).toBe(7.5);
+    });
+    test("lv100", ()=> {
+        skill = new Skill.Skill();
+        skill.effects.push(new Skill.effect.Damage(Skill.DamageType.Physical,5, skill));
+        skill.attrType = "Normal";
+
+        srcIdol.LV = 100;
+        srcIdol.ATK = 0;
+        destIdol.DEF = 0;
+
+        destIdol.attrType = ["Normal"];
+        expect(destIdol.attrType).not.toBe(undefined);
+        
+        Skill.apply(skill, srcIdol, destIdol);
+        expect(skill.applyResult["finalDamage"]).not.toBe(undefined);
+        expect(skill.applyResult["amount"]).toBe(5);
+    
+        expect(destIdol.HP).toBe(5);
+        
+    });
+    test("physical", ()=> {
+        skill = new Skill.Skill();
+        skill.effects.push(new Skill.effect.Damage(Skill.DamageType.Physical,5, skill));
+        skill.attrType = "Normal";
+
+        srcIdol.LV = 100;
+        srcIdol.ATK = 10;
+        destIdol.DEF = 0;
+
+        destIdol.attrType = ["Normal"];
+        expect(destIdol.attrType).not.toBe(undefined);
+        
+        Skill.apply(skill, srcIdol, destIdol);
+        expect(skill.applyResult["finalDamage"]).not.toBe(undefined);
+        expect(skill.applyResult["amount"]).toBe(15);
+    
+        expect(destIdol.HP).toBe(-5);
+    });
+    test("special", ()=> {
+        skill = new Skill.Skill();
+        skill.effects.push(new Skill.effect.Damage(Skill.DamageType.Special,5, skill));
+        skill.attrType = "Normal";
+
+        srcIdol.LV = 100;
+        srcIdol.SPE = 10;
+        destIdol.SDF = 0;
+
+        destIdol.attrType = ["Normal"];
+        expect(destIdol.attrType).not.toBe(undefined);
+        
+        Skill.apply(skill, srcIdol, destIdol);
+        expect(skill.applyResult["finalDamage"]).not.toBe(undefined);
+        expect(skill.applyResult["amount"]).toBe(15);
+    
+        expect(destIdol.HP).toBe(-5);
+    });
+    test("must deal 1 dmg", ()=> {
+        skill = new Skill.Skill();
+        skill.effects.push(new Skill.effect.Damage(Skill.DamageType.Special,120, skill));
+        skill.attrType = "Normal";
+
+        srcIdol.LV = 10;
+        srcIdol.SPE = 0;
+        destIdol.baseHP = 10;
+        destIdol.HP = 10;
+        destIdol.SDF = 100;
+
+        destIdol.attrType = ["Normal"];
+        expect(destIdol.attrType).not.toBe(undefined);
+        
+        Skill.apply(skill, srcIdol, destIdol);
+        expect(skill.applyResult["finalDamage"]).not.toBe(undefined);
+        expect(destIdol.HP).not.toBeGreaterThan(destIdol.baseHP);
+        expect(destIdol.HP).toBe(destIdol.baseHP - 1);
+    });
+});
+
+describe("interact with battle", ()=> {
+    var idolBattle;
+
+    beforeEach(()=>{
+        idolBattle = new IdolBattle.IdolBattle();
+    });
+
+    test("apply weather Effect", function() {
+        var skill = new Skill.Skill();
+        var weatherEffect = new Skill.effect.Weather(idolBattle,
+            {
+                type: {},
+                turn: 5
+            });
+        skill.effects.push(weatherEffect);
+
+        Skill.apply(skill);
+
+        expect(idolBattle.effects.length).not.toBe(0);
+    });
+});
+
+describe("interact with special condition", ()=> {
+
+});
