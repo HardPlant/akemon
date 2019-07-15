7cba9223153f63c27a84b196ddc647c32f7c9aec
HardPlant <abc7988se@naver.com>
2019-07-15 15:40:06 +0900

    Update stat-editor.js

--- model/stat-editor/stat-editor.js
+++ model/stat-editor/stat-editor.js
@@ -1,4 +1,4 @@
-function Stat(statObj) {
+function BaseStat(statObj) {
     this.LV = statObj.LV;
     this.HP = statObj.HP;
     this.ATK = statObj.ATK;
@@ -8,4 +8,37 @@ function Stat(statObj) {
     this.SPD = statObj.SPD;
     this.EXP = statObj.EXP;
     this.friendship = statObj.friendship;
+}
+
+function realStat(doll, battle) {
+    if (!(doll && battle
+            && doll.ability
+            && doll.statModifiers
+            && battle.weather)) {
+        console.error("[realStat] Unexpected Param");
+        console.error(this);
+    }
+    var lv = doll.baseStat.LV;
+    var statObj = {
+        LV: lv,
+        HP: calculateHP(doll.baseStat.HP,lv),
+        ATK: calculateStat(doll.baseStat.ATK,lv),
+        DEF: calculateStat(doll.baseStat.DEF,lv),
+        SAT: calculateStat(doll.baseStat.SAT,lv),
+        SDF: calculateStat(doll.baseStat.SDF,lv),
+        SPD: calculateStat(doll.baseStat.SPD,lv),
+    }
+    if (doll.nature) {
+        statObj = calculateNature(doll, statObj);
+    }
+    return statObj;
+}
+
+function calculateHP(base, lv){
+    return Math.floor(
+        ((base * 2) * lv / 100)) + 10;
+}
+function calculateStat(base, lv){
+    return Math.floor(
+        ((base) * 2 ) * lv / 100) + 5;
 }
