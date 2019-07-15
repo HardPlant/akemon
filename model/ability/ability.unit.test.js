3b604940ac240353561446d7c3bc195b02303709
HardPlant <abc7988se@naver.com>
2019-07-15 16:45:54 +0900

    Feature ability (#35)
    * Create ability.js
    
    * Create ability.unit.test.js

---
+++ model/ability/ability.unit.test.js
@@ -0,0 +1,21 @@
+
+it("위협", function() {
+    var intimate = new Ability();
+    intimate.onBattleEngage = function(battle) {
+        //console.log("");
+        //Show Effect
+        battle.enemySet.forEach() {
+
+        };
+    };
+});
+
+it("복슬복슬", function() {
+
+});
+it("짖궂은마음", function() {
+
+});
+it("위협", function() {
+
+});
