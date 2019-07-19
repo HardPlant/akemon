e0c9abe5c83799e33f3fbc8ab110bb9daa589a77
HardPlant <abc7988se@naver.com>
2019-07-19 16:25:03 +0900

    Create effect.unit.test.js

---
+++ model/stat-editor/effect.unit.test.js
@@ -0,0 +1,37 @@
+
+
+beforeAll(function() {
+
+});
+
+it("diffs rank", function() {
+
+});
+
+it("one-hit ko", function() {
+
+});
+
+it("cause status", function() {
+
+});
+
+it("diffs weather", function() {
+
+});
+
+it("faint with enemy", function() {
+
+});
+
+it("causes fixed damage", function() {
+
+});
+
+it("hits multiple time", function() {
+
+});
+
+it("checks other dolls, and..", function() {
+
+});
