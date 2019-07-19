f641de4ded37a0c66e132eea92d7a2987a9283f9
HardPlant <abc7988se@naver.com>
2019-07-19 16:24:45 +0900

    Create effect.js

---
+++ model/stat-editor/effect.js
@@ -0,0 +1,37 @@
+function Effect() {
+    this.name = name;
+    this.turn = turn;
+    this.onBeforeHit = undefined;
+    this.onHit = undefined;
+    this.onAfterHit = undefined;
+}
+
+function RankEffect() {
+    var effect = new Effect();
+    
+    return effect;
+}
+
+function OneHitKOEffect() {
+    var effect = new Effect();
+    
+    return effect;
+}
+
+function StatusEffect() {
+    var effect = new Effect();
+    
+    return effect;
+}
+
+function FixedDamageEffect() {
+    var effect = new Effect();
+    
+    return effect;
+}
+
+function HitMultipleEffect() {
+    var effect = new Effect();
+    
+    return effect;
+}
