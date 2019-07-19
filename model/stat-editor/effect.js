d1b68aaf7e9c421189b10e6d61b9355b9cd7a4e2
HardPlant <abc7988se@naver.com>
2019-07-19 16:37:45 +0900

    Update effect.js

--- model/stat-editor/effect.js
+++ model/stat-editor/effect.js
@@ -1,37 +1,56 @@
-function Effect() {
-    this.name = name;
+function Effect(turn) {
     this.turn = turn;
     this.onBeforeHit = undefined;
     this.onHit = undefined;
     this.onAfterHit = undefined;
 }
 
-function RankEffect() {
+function RankEffect(stat, amount) {
+    var effect = new Effect(-1);
+    effect.onHit = function(destDoll) {
+        destDoll.ranks[stat] += amount;
+
+        if (destDoll.ranks[stat] > 6)
+            destDoll.ranks[stat] = 6;
+        if (destDoll.ranks[stat] < -6)
+            destDoll.ranks[stat] = -6;
+
+    };
+    return effect;
+}
+
+function OneHitKOEffect(amount) {
     var effect = new Effect();
+
+    effect.onHit = function(destDoll) {
+        destDoll.HP = 0;
+        destDoll.event.onOneHitKO();
+    }
+
+    return effect;
+}
+
+function StatusEffect(prob, status, turn) {
+    var effect = new Effect();
+    effect.onHit = function(destDoll) {
+        destDoll.status = new Status(status, turn);
+    };
     
     return effect;
 }
 
-function OneHitKOEffect() {
+function FixedDamageEffect(amount) {
     var effect = new Effect();
     
+    effect.onHit = function(destDoll) {
+        destDoll.HP -= amount;
+    }
+
     return effect;
 }
 
-function StatusEffect() {
+function NothingHappenedEvent() {
     var effect = new Effect();
-    
-    return effect;
-}
 
-function FixedDamageEffect() {
-    var effect = new Effect();
-    
-    return effect;
-}
-
-function HitMultipleEffect() {
-    var effect = new Effect();
-    
     return effect;
 }
