4d872924a11737c35b371e12358fc0aa9b266b93
HardPlant <abc7988se@naver.com>
2019-07-26 11:35:15 +0900

    Create DollSelect_Plugin.js

---
+++ view/rpgmaker/DollSelect_Plugin.js
@@ -0,0 +1,156 @@
+/*:
+ * @plugindesc A Pokemon-Style Battle Plugin.
+ * @author HardPlant
+ * 
+ * @help
+ * Plugin Command:
+ *  DollBattle
+ *  DollBattle 
+ */
+(function() {
+    'use strict';
+    var pluginName = "DollSelect";
+    var _Game_Interpreter_pluginCommand = _Game_Interpreter.prototype.pluginCommand;
+    
+    var parameters = PluginManager.parameters(pluginName);
+
+    Game_Interpreter_pluginCommand = function(command, args) {
+        _Game_Interpreter_pluginCommand.call(this, command, args);
+        if (command === pluginName) {
+            switch(args[0]) {
+                case 'open':
+                    SceneManager.push(Scene_EmployActor)
+                    break;
+    
+            }
+        }
+    }
+
+    function Scene_EmployActor() {
+        this.initialize.apply(this, arguments);
+    }
+
+    Scene_EmployActor.prototype = Object.create(Scene_MenuBase.prototype);
+    Scene_EmployActor.prototype.constructor = Scene_EmployActor;
+
+    Scene_EmployActor.prototype.initialize = function() {
+        Scene_MenuBase.prototype.initialize.call(this);
+    }
+
+    Scene_EmployActor.prototype.create = function() {
+        Scene_MenuBase.prototype.create.call(this);
+        
+        this.createIndexWindow();
+        this.createGoldWindow();
+        this.createCommandWindow();
+
+        this.activateIndexWindow();
+    }
+
+    Scene_EmployActor.prototype.createIndexWindow = function() {
+        var wx = 0;
+        var wy = 0;
+        var ww = Graphics.width / 3;
+        var wh = Graphics.height;
+
+        this._indexWindow = new Window_EmployActorIndex(wx, wy, ww, wh);
+        this._indexWindow.setHandler("ok", this.onIndexOk.bind(this));
+        this._indexWindow.setHandler("cancel", this.onIndexCancel.bind(this));
+
+        this.addWindow(this._indexWindow);
+    }
+
+    Scene_EmployActor.prototype.createGoldWindow = function() {
+        this._goldWindow = new Window_Gold(0, 0);
+
+        var wx = Graphics.width - this._goldWindow.width;
+        this._goldWindow.move(wx, 0, this._goldWindow.width, this._goldWindow.height);
+
+        this.addWindow(this._goldWindow);
+    }
+
+    Scene_EmployActor.prototype.createCommandWindow = function() {
+        this._commandWindow = new Window_EmployCommand(0, 0);
+
+        var wx = Graphics.width - this._commandWindow.width;
+        var wy = Graphics.height - this._commandWindow.height;
+
+        this._commandWindow.move(wx, wy, this._commandWindow.width, this._commandHeight);
+        this._commandWindow.setHandler('employ', this.onCommandEmploy.bind(this));
+        this._commandWindow.setHandler('cancel', this.onCommandCancel.bind(this));
+        
+        this.hideCommandWindow();
+
+        this.addWindow(this._commandWindow);
+    }
+
+    Scene_EmployActor.prototype.onIndexOk = function () {
+        this.activateCommandWindow();
+    }
+
+    Scene_EmployActor.prototype.onIndexCancel = function() {
+        this.popScene();
+    }
+
+    Scene_EmployActor.prototype.onCommandEmploy = function() {
+        this.hideCommandWindow();
+        this.employ();
+        this.activateIndexWindow();
+    }
+
+    Scene_EmployActor.prototype.onCommandCancel = function() {
+        this.hideCommandWindow();
+        this._indexWindow.activate();
+    }
+
+    Scene_EmployActor.prototype.employ = function() {
+
+    };
+
+    Scene_EmployActor.prototype.activateIndexWindow = function() {
+        this._indexWindow.activate();
+        this._indexWindow.refresh();
+    }
+
+    Scene_EmployActor.prototype.activateCommandWindow = function() {
+        this._commandWindow.activate();
+        this._commandWindow.select(0);
+        this._commandWindow.show();
+    }
+
+    Scene_EmployActor.prototype.hideCommandWindow = function() {
+        this._commandWindow.deselect();
+        this._commandWindow.deactive();
+        this._commandWindow.hide();
+    }
+
+    function Window_EmployActorIndex() {
+        this.initialize.apply(this.arguments);
+    }
+
+    Window_EmployActorIndex.prototype = Object.create(Window_Selectable.prototype);
+    Window_EmployActorIndex.prototype.constructor = Window_EmployActorIndex;
+
+    Window_EmployActorIndex.prototype.initialize = function(x, y, width, height) {
+        Window_Selectable.prototype.initialize.call(this, x, y, width, height);
+
+        this.refresh();
+        this.select(0);
+        this.activate();
+    };
+
+    function Window_EmployCommand() {
+        this.initialize.apply(this, arguments);
+    }
+
+    Window_EmployCommand.prototype = Object.create(Window_Command.prototype);
+    Window_EmployCommand.prototype = Window_EmployCommand;
+
+    Window_EmployCommand.prototype.initialize = function(x, y) {
+        Window_Command.prototype.initialize.call(this, x, y);
+    };
+
+    Window_EmployCommand.prototype.makeCommandList = function() {
+        this.addCommand("Employ", "employ", true);
+    }
+})();
